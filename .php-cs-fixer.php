b8f4346232556f2a26d9d9630f90cff7e4edc9da
Renan Luiz Vendramini <rlvendramini@users.noreply.github.com>
2026-07-02 19:04:12 -0300

    Chore/php version upgrade and linter setup (#2)
    * :recycle: Implement PHP-CS-Fixer configuration, update dependencies, and enhance password generation class
    
    - Added .php-cs-fixer.php for code style enforcement.
    - Updated .gitignore to exclude cache files.
    - Modified composer.json to require PHP 8.0+, added development dependencies for PHPUnit, PHP-CS-Fixer, and PHPStan.
    - Created phpstan.neon for static analysis configuration.
    - Added phpunit.xml for PHPUnit configuration.
    - Refactored RandomPassword class for improved readability and performance.
    - Introduced comprehensive tests for RandomPassword class functionality.
    
    * :♻️ Refactor password generator with secure shuffling and CRNG

---
+++ .php-cs-fixer.php
@@ -0,0 +1,52 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->name('*.php');
+
+$config = new PhpCsFixer\Config();
+return $config
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP84Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'default' => 'single_space',
+        ],
+        'blank_line_after_namespace' => true,
+        'blank_line_after_opening_tag' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['return', 'try'],
+        ],
+        'cast_spaces' => true,
+        'class_attributes_separation' => [
+            'elements' => [
+                'method' => 'one',
+                'property' => 'one',
+            ],
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
+        'function_typehint_space' => true,
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+        ],
+        'no_empty_statement' => true,
+        'no_leading_import_slash' => true,
+        'no_trailing_whitespace' => true,
+        'no_unused_imports' => true,
+        'no_whitespace_in_blank_line' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'return_type_declaration' => ['space_before' => 'none'],
+        'single_blank_line_at_eof' => true,
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+        'visibility_required' => [
+            'elements' => ['method', 'property'],
+        ],
+        'whitespace_after_comma_in_array' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setUnsupportedPhpVersionAllowed(true)
+    ->setFinder($finder);
